3545e1de2fc0d437a26a2a1d0d921b67ee142724
Sam Jones <samueldjones24@gmail.com>
2019-03-06 21:13:04 +0000

    Refactored arrays

--- src/arrays.js
+++ src/arrays.js
@@ -1,4 +1,3 @@
-// tidy up
 const getNthElement = (index, array) => {
   if (index < array.length) {
     return array[index];
@@ -32,20 +31,11 @@ const numbersToStrings = (numbers) => {
 };
 
 const uppercaseWordsInArray = (strings) => {
-  // you can write your map function calbacks using arrow notation as well, e.g:
-  /*
   return strings.map(string => string.toUpperCase());
-  */
-  return strings.map(function upperCase(string) {
-    return string.toUpperCase();
-  });
 };
 
 const reverseWordsInArray = (strings) => {
-  // same here, you can use arrow function notation with your map callbacks
-  return strings.map(function reverseString(string) {
-    return string.split('').reverse().join('');
-  });
+  return strings.map(string => string.split('').reverse().join(''));
 };
 
 const onlyEven = (numbers) => {
@@ -57,18 +47,9 @@ const removeNthElement2 = (index, array) => {
 };
 
 const elementsStartingWithAVowel = (strings) => {
-  // another way of doing this would be to define a list of vowels in an array
-  // then you can check if character: string[0].toLowerCase() is one of the items in the vowels array
-  /*
-  const vowels = ['a', 'e', 'i', 'o', 'u']
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
   return strings.filter(string => {
-    return vowel.includes(string[0].toLowerCase());
-  });
-  another interesting approach is to use a regex to do pattern matching, Russell did this in his codebase:
-  see: https://github.com/ruzla/javascript-basics/blob/master/src/arrays.js#L51
-  */
-  return strings.filter(string => {
-    return string[0].toLowerCase() === 'a' || string[0].toLowerCase() === 'e' || string[0].toLowerCase() === 'i' || string[0].toLowerCase() === 'o' || string[0].toLowerCase() === 'u';
+    return vowels.includes(string[0].toLowerCase());
   });
 };
 
@@ -80,7 +61,6 @@ const sumNumbers = (numbers) => {
   return numbers.reduce((a, b) => a + b, 0);
 };
 
-// tidy up & revise Unicode values
 const sortByLastLetter = (strings) => {
   return strings.sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
 };
